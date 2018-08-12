5b692c22853498f02df13ff031a86bd2f9538c54
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-12 18:37:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {artist: "Name", title: "Song"};
+var playlist = {[artistName]: [songTitle]};
 
 function updatePlaylist(playlist,artistName,songTitle) {
   Object.assign({}, playlist, { [artistName]: songTitle })
